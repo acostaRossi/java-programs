f163c4e39b4f9429ae9c69df33f37b164c23ad4a
DESKTOP-JQ7HNAN\Alberto <acosta@itisrossi.vi.it>
2020-11-15 14:35:21 +0100

    Hello world with Java

---
+++ HelloWorld/src/com/didattica/Hello.java
@@ -0,0 +1,11 @@
+package com.didattica;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world\n");
+		
+		System.out.println("Primo parametro: " + args[0]);
+		System.out.println("Secondo parametro: " + args[1]);
+	}
+}
